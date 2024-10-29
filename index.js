622169aa271ebd0959e947d052e9e9e50f4b4562
Marc Bien Go <marcbiengo@gmail.com>
2024-10-29 13:22:10 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var drumCount = document.querySelectorAll(".drum").length;
+for (var i = 0; i < drumCount; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonContent = this.innerHTML;
+        makeSound(buttonContent);
+        buttonAnimation(buttonContent);
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(key) {
+    var activeButton = document.querySelector("." + key);
+    if (activeButton != null) {
+        activeButton.classList.add("pressed");
+        setTimeout(function () {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
+}
